8de5eb0addf988df2299db5a50d8739f2e322c83
Gelson Higino <48499214+theghhz@users.noreply.github.com>
2024-09-21 23:44:16 -0300

    Update desafio-5.c

--- desafios/desafio-5.c
+++ desafios/desafio-5.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
 
-void bomba(int estado1[], int estado2[]){
-    printf("\nestado1 ");
-    for(int i=0; i<3; i++){
-        printf("[%d]", estado1[i]);
-    }
-    printf("\nestado2 ");
-    for(int i=0; i<3; i++){
-        printf("[%d]", estado2[i]);
-    }
-}
-
 int main() {
-    int estado1[3]; // Estado das lâmpadas na primeira etapa
-    int estado2[3]; // Estado das lâmpadas na segunda etapa
+    int estado1[3]; 
+    int estado2[3]; 
 
-    // 1ª Etapa
+    
     printf("1ª ETAPA\n");
     printf("INTERRUPTOR 1: LIGADO\n");
     printf("INTERRUPTOR 2: DESLIGADO\n");
@@ -27,9 +16,7 @@ int main() {
         scanf("%d", &estado1[i]);
     }
     
-    bomba(estado1,estado2);
-    
-    // 2ª Etapa
+   
     printf("\n2ª ETAPA\n");
     printf("INTERRUPTOR 1: DESLIGADO\n");
     printf("INTERRUPTOR 2: DESLIGADO\n");
@@ -41,9 +28,7 @@ int main() {
         scanf("%d", &estado2[i]);
     }
     
-    bomba(estado1,estado2);
-    
-    // Identificando qual interruptor controla qual lâmpada
+  
     for (int i = 0; i < 3; i++) {
         if ((estado1[i] == 1 && estado2[i] == 0) || estado1[i] == 1 && estado2[i] == 1) {
             printf("\nINTERRUPTOR 1 controla a LÂMPADA %d\n", i + 1);
